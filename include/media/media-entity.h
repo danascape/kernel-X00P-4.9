81091b54607b08192c2c77689feaf4ccdc81e3e2
Artem Borisov <dedsa2002@gmail.com>
2018-11-06 13:00:54 +0400

    HACK: media: Restore old entity_id logic
    3.18 camera doesn't know how to use the new unique ids and
    so doesn't enumerate the entities properly.
    
    Change-Id: If566a7527263c995ab201aa5271c1be46aa7285f

--- include/media/media-entity.h
+++ include/media/media-entity.h
@@ -260,6 +260,7 @@ enum media_entity_type {
  */
 struct media_entity {
 	struct media_gobj graph_obj;	/* must be first field in struct */
+	u32 id;
 	const char *name;
 	enum media_entity_type obj_type;
 	u32 function;
@@ -333,7 +334,7 @@ struct media_intf_devnode {
  */
 static inline u32 media_entity_id(struct media_entity *entity)
 {
-	return entity->graph_obj.id;
+	return entity->id;
 }
 
 /**
